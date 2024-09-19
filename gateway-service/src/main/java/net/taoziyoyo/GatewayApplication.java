5e11a6e89f693cb817440dd1123f6134b9d07be8
taoziyoyo2566 <taoziyoyo2566@gmail.com>
2024-09-20 01:44:29 +0800

    spring cloud eureka

--- gateway-service/src/main/java/net/taoziyoyo/GatewayApplication.java
+++ gateway-service/src/main/java/net/taoziyoyo/GatewayApplication.java
@@ -2,7 +2,10 @@ package net.taoziyoyo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+
+//@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
--- gateway-service/src/main/java/net/taoziyoyo/config/CorsConfig.java
+++ gateway-service/src/main/java/net/taoziyoyo/config/CorsConfig.java
@@ -1,32 +1,32 @@
-package net.taoziyoyo.config;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Profile;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.Collections;
-/*
-仅在stage环境下使用java配置，dev使用yaml配置
- */
-@Profile("stage")
-@Configuration
-public class CorsConfig {
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration config = new CorsConfiguration();
-//        config.setAllowedOrigins(Collections.singletonList("*")); // Using set instead of add
-        config.setAllowedOrigins(Collections.singletonList("http://154.23.243.155:3000")); // 允许特定的React地址
-
-        config.setAllowedMethods(Collections.singletonList("*"));
-        config.setAllowedHeaders(Collections.singletonList("*"));
-        config.setAllowCredentials(true); // If needed for credentials
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", config);
-
-        return new CorsWebFilter(source);
-    }
-}
+//package net.taoziyoyo.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.context.annotation.Profile;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//
+//import java.util.Collections;
+///*
+//仅在stage环境下使用java配置，dev使用yaml配置
+// */
+//@Profile("stage")
+//@Configuration
+//public class CorsConfig {
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration config = new CorsConfiguration();
+////        config.setAllowedOrigins(Collections.singletonList("*")); // Using set instead of add
+//        config.setAllowedOrigins(Collections.singletonList("http://154.23.243.155:3000")); // 允许特定的React地址
+//
+//        config.setAllowedMethods(Collections.singletonList("*"));
+//        config.setAllowedHeaders(Collections.singletonList("*"));
+//        config.setAllowCredentials(true); // If needed for credentials
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", config);
+//
+//        return new CorsWebFilter(source);
+//    }
+//}
